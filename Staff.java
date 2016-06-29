d27aaa70d0bbff3a98d368e3dca8d494351fe923
president47 <siramirreisosadat97@gmail.com>
2016-06-29 21:35:43 +0430

    changes
    salary means monthly payment
    in chand ta class ro neveshtam felan

--- Staff.java
+++ Staff.java
@@ -5,6 +5,26 @@ public class Staff {
 	int socialNumber;
 	int personnelNumber;
 	int jobTitle;
-	int montlySalaty;
-
+	int Salary;
+}
+public class Manager extends Staff {
+	int attendedMeatings;
+	int assistantsNeeded;
+	String driver'sSpecifications;
+	String car'sModel;
+}
+public class Assistant extends Staff {
+	String assistantTitle;
+	int doneTasks;
+	int attendedMeatings;
+	int paymentPerHour;
+}
+public class HighRankingEmployee extends Staff{
+	int hoursOfVacation;
+	int paymentPerOvertimeHour;
+	int overtimeHour;
+}
+public class LowRankingEmployee extends Staff{
+	int solvedPuzzle;
+	int paymentPerOvertimeHour;
 }
